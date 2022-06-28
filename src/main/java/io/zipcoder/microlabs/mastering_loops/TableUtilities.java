9b2fe102884f4c141e890bd5fdfab5351107125e
jd <j.judd.singer@gmail>
2022-06-28 12:03:00 -0400

    completed the table utilities but still having alignment issues

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -19,10 +19,41 @@ public class TableUtilities {
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
-    }
 
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= 10; i++){
+            for (int j = 1; j <= 10; j++){
+                if (i * j < 10){
+                    sb.append((i * j) + " | ");
+                } else {
+                    sb.append((i * j) + " | ");
+                }
+
+            }
+            sb.append("\n");
+        }
+
+
+        return sb.toString();
+    }
+//having trouble aligning the table but the logic is there
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= tableSize; i++){
+            for (int j = 1; j <= tableSize; j++){
+                if (i * j < 10){
+                    sb.append((i * j) + " | ");
+                } else {
+                    sb.append((i * j) + " | ");
+                }
+
+            }
+            sb.append("\n");
+        }
+
+
+        return sb.toString();
     }
 }
